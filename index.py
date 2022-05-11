a79ee54bbdcba2a14f97c3581a77415dfd135471
Pascal Opitz <contact@pascalopitz.com>
2022-05-11 21:15:30 +0930

    Better sigint, dealing with 0 values

--- index.py
+++ index.py
@@ -1,53 +1,80 @@
 import asyncio
 import json
 import os
+import signal
+import sys
 from urllib import request
-from PyP100 import PyP100
+
 from dotenv import load_dotenv
+from PyP100 import PyP100
 
 load_dotenv()
+
+timeout = 10
 
 def get_grid_consumption():
     url = 'http://{ip}/solar_api/v1/GetPowerFlowRealtimeData.fcgi'.format( ip=os.getenv('FRONIUS_IP') )
     f = request.urlopen(url)
     s = json.loads(f.read().decode('utf-8'))
-    grid_pull = float(s['Body']['Data']['Site']['P_Grid'])
-    pv = float(s['Body']['Data']['Site']['P_PV'])
-    load = float(s['Body']['Data']['Site']['P_Load'])
+    grid_pull = float(s['Body']['Data']['Site']['P_Grid'] or 0)
+    pv = float(s['Body']['Data']['Site']['P_PV'] or 0)
+    load = float(s['Body']['Data']['Site']['P_Load'] or 0)
     surplus = pv + load
     print('pv={pv}, load={load}, surplus={surplus}, grid_pull={grid_pull}'.format( pv=pv, load=load, surplus=surplus, grid_pull=grid_pull ))
     return [grid_pull, surplus]
 
+async def interation(p100, on_threshold, off_threshold, status):
+    while True:
+        try:
+            [grid_pull, surplus] = get_grid_consumption()
+            new_status = (status == True and grid_pull < off_threshold) or (status == False and surplus > on_threshold)
 
-async def main():
-    p100 = PyP100.P100(os.getenv('TAPO_SOCKET_IP'), os.getenv('TAPO_EMAIL'), os.getenv('TAPO_PASSWORD')) 
+            if status != new_status:
+                if new_status == False:
+                    print('turnOff')
+                    p100.turnOff()
+                else:
+                    print('turnOn')
+                    p100.turnOn()
+
+            status = new_status
+        except BaseException as err:
+            print(f"Unexpected {err=}, {type(err)=}")
+
+        await asyncio.sleep(timeout)
+
+def main():
+    loop = asyncio.new_event_loop()
+
+    p100 = PyP100.P100(os.getenv('TAPO_SOCKET_IP'), os.getenv('TAPO_EMAIL'), os.getenv('TAPO_PASSWORD'))
     p100.handshake()
     p100.login()
     info = p100.getDeviceInfo()
     status = info['result']['device_on']
-    on_threshold = float(os.getenv('DEVICE_WATTAGE')) - float(os.getenv('GRID_PULL_WATTAGE_MAX')) 
+    print('Device on={status}'.format(status=status))
+    on_threshold = float(os.getenv('DEVICE_WATTAGE')) - float(os.getenv('GRID_PULL_WATTAGE_MAX'))
     off_threshold = float(os.getenv('GRID_PULL_WATTAGE_MAX'))
 
-    while True:
-        [grid_pull, surplus] = get_grid_consumption()
-        new_status = (status == True and grid_pull < off_threshold) or (status == False and surplus > on_threshold)
+    async def exit():
+        print('turnOff')
+        p100.turnOff()
+        print("Stop")
+        loop.stop()
 
-        if status != new_status:
-            if new_status == False:
-                print('turnOff')
-                p100.turnOff()
-            else:
-                print('turnOn')
-                p100.turnOn()
+    def ask_exit():
+        task.cancel()
+        asyncio.ensure_future(exit())
 
-    
-        status = new_status
-        await asyncio.sleep(1)
+    for sig in (signal.SIGINT, signal.SIGTERM):
+        loop.add_signal_handler(sig, ask_exit)
 
-loop = asyncio.get_event_loop()
-task = loop.create_task(main())
+    task = loop.create_task(interation(p100=p100, on_threshold=on_threshold, off_threshold=off_threshold, status=status))
 
-try:
-    loop.run_until_complete(task)
-except asyncio.CancelledError:
-    pass
+    try:
+        loop.run_until_complete(task)
+    except asyncio.CancelledError:
+        pass
+
+
+if __name__ == "__main__":
+    main()
